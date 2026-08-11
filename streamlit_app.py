c9b527789c336b1a8da766d553fdb9a6ce206c66
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2026-08-11 07:27:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
+
+gapm = px.data.gapminder()
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    gapm
 )
